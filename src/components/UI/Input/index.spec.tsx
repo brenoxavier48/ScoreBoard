4667d69fb79f89b29aca2a45c9457660107a9fd2
Breno Henrique Martins Xavier <breno.xavier@MacBook-Pro-de-Invillia.local>
2020-10-28 20:44:35 -0300

    test: input component

--- src/components/UI/Input/index.spec.tsx
+++ src/components/UI/Input/index.spec.tsx
@@ -3,13 +3,20 @@ import { create, act } from 'react-test-renderer';
 import { fireEvent, render } from '@testing-library/react-native'
 import InputUI from './index'
 
-describe('Button Component tests', () => {
+describe('Input Component tests', () => {
 
   test('Should has correctly accessibility label', async () => {
-    
+    const LABEL = "input-test"
+    const { getByA11yLabel } = render(<InputUI label={LABEL} value="" handleChangeText={() => {}} ></InputUI>)
+    const input = getByA11yLabel('input-test')
+    expect(input.props.accessibilityLabel).toBe(LABEL)
   })
 
-  test('Should calls haandlePress correctly', async () => {
-    
+  test('Should calls handleChangeText correctly', async () => {
+    const mockHandleChangeText = jest.fn()
+    const { getByA11yLabel } = render(<InputUI label="input-test" value="" handleChangeText={mockHandleChangeText} ></InputUI>)
+    const input = getByA11yLabel('input-test')
+    fireEvent.changeText(input, 'test')
+    expect(mockHandleChangeText).toBeCalled()
   })
 })
