be25af0217bd29dcb4a27e8836cb8ee8ce6cf2a2
Breno Henrique Martins Xavier <brenoxavier8.bx@gmail.com>
2020-11-29 21:26:56 -0300

    feat: add selectOneScore

--- src/store/Score/score.selectors.ts
+++ src/store/Score/score.selectors.ts
@@ -1,5 +1,9 @@
 import { ActionStore } from '../protocols'
 import { rootReducer } from '../index'
+import { ScoreModel } from '../../domain/models/scoreModel'
 
 export const selectAllScores = (state: any) => state.score
 
+export const selectOneScore = (id: number) => {
+  return (state: any) => state.score.filter((score: ScoreModel) => score.id === id)
+}
